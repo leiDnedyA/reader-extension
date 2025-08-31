0aae31c5fd56a41ecfa1b3883eca19b7bcb5e30f
ayden <aydendiel@gmail.com>
2025-08-31 13:57:21 -0400

    feat: added continued reading

--- content.js
+++ content.js
@@ -9,9 +9,19 @@ const currState = {
   utterance: null
 };
 
+const clearState = () => {
+  currState.target.style.background = "";
+  currState.target = null;
+  currState.utterance = null;
+  currState.utterance?.removeEventListener("end");
+}
+
 document.addEventListener('keydown', async (event) => {
   if (event.key === 'Escape') {
     synth.cancel();
+    if (currState?.utterance) {
+      clearState();
+    }
   }
 });
 
@@ -27,22 +37,42 @@ async function getConfig() {
   })
 }
 
-// TODO: make it auto switch to next para
+const readSingleElement = (element) => {
+  currState.target = element;
+  const text = element.textContent
+  currState.utterance = new SpeechSynthesisUtterance(text);
+  currState.target.style.background = "rgba(255, 255, 0, .3)";
+  currState.utterance.addEventListener("end", clearState)
+  synth.speak(currState.utterance);
+};
+
+const readContinuous = (firstElement) => {
+  if (currState?.utterance) {
+    currState.utterance.removeEventListener("end");
+  }
+  currState.target = firstElement;
+  const text = firstElement.textContent
+  currState.utterance = new SpeechSynthesisUtterance(text);
+  currState.target.style.background = "rgba(255, 255, 0, .3)";
+  currState.utterance.addEventListener("end", () => {
+    const prevTarget = currState.target;
+    currState.target.style.background = "";
+    currState.target = null;
+    currState.utterance = null;
+    readContinuous(prevTarget.nextElementSibling);
+  });
+  synth.speak(currState.utterance);
+};
 
 document.addEventListener('click', async (event) => {
   if (event?.target?.textContent && event.altKey) {
     const config = await getConfig();
     if (!synth.speaking && config.enabled) {
-      currState.target = event.target;
-      const text = event.target.textContent
-      currState.utterance = new SpeechSynthesisUtterance(text);
-      currState.target.style.background = "rgba(255, 255, 0, .3)";
-      currState.utterance.addEventListener("end", () => {
-        currState.target.style.background = "";
-        currState.target = null;
-        currState.utterance = null;
-      })
-      synth.speak(currState.utterance);
+      if (event.shiftKey) {
+        readContinuous(event.target);
+      } else {
+        readSingleElement(event.target);
+      }
     }
   }
 });
